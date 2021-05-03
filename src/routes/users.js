53540965a72d8d866d4a4a76895c2cb2d32dde80
QSD_hm <info@qsdbih.com>
2021-05-03 23:19:27 +0200

    When fetching user details, if the city not exist - return city_name as null.

--- src/routes/users.js
+++ src/routes/users.js
@@ -83,40 +83,51 @@ router.get('/', async (req, res) => {
 router.get('/details/:user_id', verifyToken, async (req, res) => {
   try {
     const user = await User.findById(req.params.user_id).select('-password');
-    const city = await City.findById(user.city_id);
-    const {
-      _id,
-      first_name,
-      last_name,
-      email,
-      address,
-      phone_number,
-      city_id,
-      fcm_tokens,
-      services,
-      created_at,
-      updated_at,
-    } = user;
-    res.status(200).json(
-      success(
-        'OK',
-        {
-          _id,
-          first_name,
-          last_name,
-          email,
-          address,
-          phone_number,
-          city_id,
-          fcm_tokens,
-          services,
-          created_at,
-          updated_at,
-          city_name: city.name,
-        },
-        res.statusCode,
-      ),
-    );
+
+    let cityName;
+    City.findById(user.city_id, (err, city) => {
+      if (err) {
+        if (err.value === 'null') {
+          cityName = null;
+        }
+      } else {
+        cityName = city.name;
+      }
+
+      const {
+        _id,
+        first_name,
+        last_name,
+        email,
+        address,
+        phone_number,
+        city_id,
+        fcm_tokens,
+        services,
+        created_at,
+        updated_at,
+      } = user;
+      res.status(200).json(
+        success(
+          'OK',
+          {
+            _id,
+            first_name,
+            last_name,
+            email,
+            address,
+            phone_number,
+            city_id,
+            fcm_tokens,
+            services,
+            created_at,
+            updated_at,
+            city_name: cityName,
+          },
+          res.statusCode,
+        ),
+      );
+    });
   } catch ({message}) {
     message.startsWith('Cast to')
       ? res.status(404).json(error('User not found', res.statusCode))
